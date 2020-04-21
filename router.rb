3683bbd9a0edd8e354f88dc2bf9f4f3881167f13
Damien Milon <milon.damien@gmail.com>
2020-04-21 07:55:31 +0200

    change action order

--- router.rb
+++ router.rb
@@ -20,20 +20,20 @@ class Router
     puts "--------------------"
     puts "------- MENU -------"
     puts "--------------------"
-    puts "1. List all meals"
-    puts "2. Add new meal"
-    puts "3. List all customers"
-    puts "4. Add new customer"
+    puts "1. Add new meal"
+    puts "2. List all meals"
+    puts "3. Add new customer"
+    puts "4. List all customers"
     puts "8. Exit"
     print "> "
   end
 
   def route_action(choice)
     case choice
-    when 1 then @meals_controller.list
-    when 2 then @meals_controller.add
-    when 3 then @customers_controller.list
-    when 4 then @customers_controller.add
+    when 1 then @meals_controller.add
+    when 2 then @meals_controller.list
+    when 3 then @customers_controller.add
+    when 4 then @customers_controller.list
     when 8 then stop!
     else
       puts "Try again..."
