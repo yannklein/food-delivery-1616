056af38f3136388697562ebf8cf617fab6f6dc42
Capucine Scheidel-Buchet <capucine.scheidelbuchet@gmail.com>
2020-06-12 13:46:02 +0200

    reverted app.rb and router.rb

--- router.rb
+++ router.rb
@@ -1,100 +1,46 @@
 class Router
-  def initialize(meals_controller, customers_controller, sessions_controller, orders_controller)
+  def initialize(meals_controller, customers_controller)
     @meals_controller = meals_controller
     @customers_controller = customers_controller
-    @sessions_controller = sessions_controller
-    @orders_controller = orders_controller
     @running = true
   end
 
   def run
     while @running
-      @current_user = @sessions_controller.login
-      while @current_user
-        if @current_user.manager?
-          route_manager_action
-        else
-          route_delivery_guy_action
-        end
-      end
+      print_menu
+      choice = gets.chomp.to_i
       print `clear`
+      route_action(choice)
     end
   end
 
   private
 
-  def route_manager_action
-    print_manager_menu
-    choice = gets.chomp.to_i
-    print `clear`
-    manager_action(choice)
-  end
-
-  def route_delivery_guy_action
-    print_delivery_guy_menu
-    choice = gets.chomp.to_i
-    print `clear`
-    delivery_guy_action(choice)
-  end
-
-  def print_manager_menu
+  def print_menu
     puts "--------------------"
     puts "------- MENU -------"
     puts "--------------------"
-    puts "1. Add new meal"
-    puts "2. List all meals"
-    puts "3. Add new customer"
-    puts "4. List all customers"
-    puts "5. Add new order"
-    puts "6. List all undelivered orders"
-    common_menu
-  end
-
-  def print_delivery_guy_menu
-    puts "--------------------"
-    puts "------- MENU -------"
-    puts "--------------------"
-    puts "1. List my undelivered orders"
-    puts "2. Mark order as delivered"
-    common_menu
-  end
-
-  def common_menu
-    puts "7. Logout"
+    puts "1. List all meals"
+    puts "2. Add new meal"
+    puts "3. List all customers"
+    puts "4. Add new customer"
     puts "8. Exit"
     print "> "
   end
 
-  def manager_action(choice)
+  def route_action(choice)
     case choice
-    when 1 then @meals_controller.add
-    when 2 then @meals_controller.list
-    when 3 then @customers_controller.add
-    when 4 then @customers_controller.list
-    when 5 then @orders_controller.add
-    when 6 then @orders_controller.list_undelivered_orders
-    when 7 then logout!
+    when 1 then @meals_controller.list
+    when 2 then @meals_controller.add
+    when 3 then @customers_controller.list
+    when 4 then @customers_controller.add
     when 8 then stop!
-    else puts "Try again..."
+    else
+      puts "Try again..."
     end
-  end
-
-  def delivery_guy_action(choice)
-    case choice
-    when 1 then @orders_controller.list_my_orders(@current_user)
-    when 2 then @orders_controller.mark_as_delivered(@current_user)
-    when 7 then logout!
-    when 8 then stop!
-    else puts "Try again..."
-    end
-  end
-
-  def logout!
-    @current_user = nil
   end
 
   def stop!
-    logout!
     @running = false
   end
 end
